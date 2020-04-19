7e84e4f28065f87f373433f1c2b456c1255de3f6
zhoudzj@163.com <Mark@zhoudideMacBook-Pro-2.local>
2020-04-19 23:18:28 +0800

    首页响应式布局

--- src/components/House.js
+++ src/components/House.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 import styles from "../assets/css/house.css"
 
-const House = ({ url, title, houseId }) => {
+const House = ({url, title, houseId}) => {
     return (
-        <div className={styles.house_wrap}>
-            <Link to={`/house/:${String(houseId)}`}>
-                <img className={styles.img_wrap} src={url} />
-                <span >{title}</span>
-            </Link>
-        </div>
+        <Link to={`/house/:${String(houseId)}`}>
+            <div className={styles.house_wrap}>
+                <img className={styles.img_wrap} src={url}/>
+                <span className={styles.g_house_name}>{title}</span>
+            </div>
+        </Link>
     )
 }
 
