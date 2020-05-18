96426cfe45357a6ed4d7dad05c99e6a0c9799b1a
zhoudzj@163.com <Mark@zhoudideMacBook-Pro-2.local>
2020-05-18 08:31:36 +0800

    退出登录

--- src/components/HeaderHOC.js
+++ src/components/HeaderHOC.js
@@ -1,32 +1,58 @@
 import React, {Component} from 'react';
-import {useHistory} from 'react-router-dom'
+import {useHistory, withRouter} from 'react-router-dom'
 import {ReactDOM} from 'react-router-dom'
 import styles from '../assets/css/header_hoc.css'
+import {Modal} from 'antd';
+const { confirm } = Modal;
 
 const HeaderHOC = WrappedComponent => {
-  return class extends Component {
+  class Headers extends Component {
     constructor(props) {
       super(props)
+      this.handleLogout = this
+        .handleLogout
+        .bind(this)
       this.state = {
         data: "初始化"
       };
     }
     componentWillMount() {}
     handleLogout() {
-      console.log('走了');
-      this.setState({data: "改变数据"})
+      const that = this;
+      confirm({
+        title: '确定退出登录吗?',
+        content: '成功退出后将跳转到登录界面',
+        okText:"确认",
+        cancelText:"取消",
+        onOk() {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              that.setState({data: "改变数据"});
+              resolve();
+              that
+                .props
+                .history
+                .push('/login');
+            }, 500);
+          }).catch(() => console.log('退出登录失败!'));
+        },
+        onCancel() {}
+      })
+
     }
     render() {
       return (
         <div>
           <WrappedComponent {...this.props}>
-            <div className={styles.hoc}><span className={styles.user}>用户名</span>|<span className={styles.logout}>退出</span>
+            <div className={styles.hoc}>
+              <span className={styles.user}>用户名</span>|<span className={styles.logout} onClick={this.handleLogout}>退出</span>
             </div>
           </WrappedComponent>
         </div>
       )
     }
   }
+  return withRouter(Headers)
 }
 
 export default HeaderHOC
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,18 +4,18 @@ import styles from "../assets/scss/login.scss"
 
 const Login = () => {
     const history = useHistory();
-    const haddleLogin = ()=>{
+    const haddleLogin = () => {
         history.push('/')
     }
     return (
         <div className={styles.Login}>
             <header className={styles.Login_header}>一站式智能场景选装</header>
-            {/*<div className={styles.content_wrap}>
+            <div className={styles.content_wrap}>
                 <div className={styles.input_wrap}><span>用户名</span><input/></div>
                 <div className={styles.input_wrap}><span>密码</span><input/></div>
                 <button className={styles.button_login} onClick={haddleLogin}>登录</button>
                 <div>powered by Mingou</div>
-            </div>*/}
+            </div>
         </div>
     )
 }
