b611b4aff584ca79c7a81c63524232187c2a9018
dimark <zhoudi@33.cn>
2020-06-04 20:23:02 +0800

    先清空服务端的历史文件

--- deployProd.sh
+++ deployProd.sh
@@ -2,6 +2,7 @@ server="root@39.100.127.116"
 
 path="/var/www/html/mingou"
 
+ssh ${server} "cd ${path} && rm -rf *"
 scp -r build/* ${server}:${path}
 
 echo "\033[32;5m==================已上传至服务👌==================\033[0m" 
