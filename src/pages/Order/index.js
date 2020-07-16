09362d05ef35f8234637c1437ddae10074286ce1
zhoudzj@163.com <Mark@zhoudideMacBook-Pro-2.local>
2020-07-16 08:54:24 +0800

    提交

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -18,36 +18,46 @@ const Order = ({communityId}) => {
   useEffect(() => {
     const fetchData = async() => {
       const data = await getRoomList({communityId});
-      const options = haddleData(data, 'unit', 'building');
-      console.log(options)
+      const options = haddleData(data);
       setOptions(options)
     }
     fetchData();
-    const haddleData = (data, id, pid) => {
+    const haddleData = (data) => {
       const result = [];
       console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        let buildingObj = {};
-        let unitObj = {};
-        let roomObj = {};
-        var rawObj = data[i];
-        for (var j in rawObj) {
-          if(j === 'building'){
-              rawObj[j] = buildingObj['value'];
-              rawObj[j] = buildingObj['label'];
-          }else if(j === 'unit'){
-              rawObj[j] = unitObj['value'];
-              rawObj[j] = unitObj['label'];
-              buildingObj.children.push(unitObj);
-          }else if(j === 'room'){
-              rawObj[j] = roomObj['value'];
-              rawObj[j] = roomObj['label'];
-              unitObj.children.push(roomObj);
-          }
-        }
-        result.push(buildingObj);
-      }
-      console.log(result);
+      let arrWrap = [];
+
+      var container = {};
+      data.forEach(item=>{
+        container[item.building] = container[item.building] || [];
+        container[item.building].push(item);
+      });
+
+      // for (let i = 0; i < data.length; i++) {
+      //   let buildingObj = {};
+      //   buildingObj.children = [];
+      //   let unitObj = {};
+      //   unitObj.children = [];
+      //   let roomObj = {};
+      //   var rawObj = data[i];
+      //   for (var j in rawObj) {
+      //     if(j === 'building'){
+      //         buildingObj['value'] = rawObj[j];
+      //         buildingObj['label'] = rawObj[j];
+      //     }else if(j === 'unit'){
+      //         unitObj['value'] = rawObj[j];
+      //         unitObj['label'] = rawObj[j];
+      //         buildingObj.children.push(unitObj);
+      //     }else if(j === 'room'){
+      //         roomObj['value'] = rawObj[j];
+      //         roomObj['label'] = rawObj[j];
+      //         unitObj.children.push(roomObj);
+      //     }
+      //   }
+      //   result.push(buildingObj);
+      // }
+      console.log(container);
+      // console.log(obj);
       // for (var j in obj) {   //当前元素   var current = obj[j];   //当前元素的父元素   var
       // currentParent = obj[current[pid]];   //如果当前元素存在父元素   if (currentParent) {
       // //如果当前元素的父元素没有children键     if (!currentParent['children']) {
