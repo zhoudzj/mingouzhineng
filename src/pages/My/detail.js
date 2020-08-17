83fc1f01bc043b1934fdfdf7235d3b4371f710b4
zhoudzj@163.com <Mark@zhoudideMacBook-Pro-2.local>
2020-08-17 08:12:59 +0800

    订单预览pdf

--- src/pages/My/detail.js
+++ src/pages/My/detail.js
@@ -9,7 +9,8 @@ import {
   Pagination,
   Input,
   message,
-  Space
+  Space,
+  Descriptions
 } from 'antd';
 import {getOrderListDetail,createPdf} from '@/config/api';
 import xlsx from 'xlsx';
@@ -76,8 +77,8 @@ const MyDetail = ({isShow,orderItem,handleCancel})=>{
   //   window.URL.revokeObjectURL(url);
   // }
   const getPdf = () => {
-    const domElement = document.getElementById('tab');
-    html2canvas(domElement).then((canvas)=>{
+    const domElement = document.getElementsByClassName('ant-modal-body');
+    html2canvas(domElement[0]).then((canvas)=>{
         let contentWidth = canvas.width;
         let contentHeight = canvas.height;
 
@@ -93,10 +94,8 @@ const MyDetail = ({isShow,orderItem,handleCancel})=>{
 
   
   useEffect(() => {
-
     const fetchData = async () => {
       const rawData = await getOrderListDetail({orderId:String(orderItem.id)});
-      console.log(rawData);
       setTableData(rawData);
     }
     if(isShow){
@@ -106,56 +105,28 @@ const MyDetail = ({isShow,orderItem,handleCancel})=>{
     }
   }, [isShow]);
 
-  const submit = useCallback(() => {
+  const callbackHandler = useCallback(() => {
     let totalPrice = 0;
-    let arr = tableData.map((item, index) => {
-      totalPrice += item.price*item.number;
-      return {
-          '产品名称': item.name,
-          '颜色': getColor(item.color),
-          '价格': item.price,
-          '数量': item.number
-        }
-    });
-    const topHeader = {
-      A1:{t:'s',v:'项目名称'},
-      B1:{t:'s',v:`${orderItem.project_name}`},
-      A2:{t:'s',v:`销售姓名:${orderItem.sales_name}`},
-      B2:{t:'s',v:`姓名:${orderItem.master_name}`},
-      C2:{t:'s',v:`房号:${orderItem.house_num}`},
-      D2:{t:'s',v:`户型:${orderItem.style_name}`},
-      A3:{t:'s',v:`总价:${totalPrice}`},
-      B3:{t:'s',v:`元`}
+    for(const item of tableData){
+      totalPrice += (Number(item.price) * item.number);
     }
-    const _headers = ['产品名称','颜色','价格','数量']
-    let headers = _headers.map((v,i)=>Object.assign({},{v:v,t:'s',position:String.fromCharCode(65+i)+4})).reduce((prev,next) => Object.assign({},prev,{[next.position]:{v:next.v,t:next.t}}),{});
-    let data = arr.map((v,i)=>_headers.map((k,j)=>{ let type = '';if(k==='产品名称'||k==='颜色'){type='s'}else{type='n'}  return Object.assign({},{v:v[k],t:type,position:String.fromCharCode(65+j)+(i+5)}) })).reduce((prev,next)=>prev.concat(next)).reduce((prev,next)=>Object.assign({},prev,{[next.position]:{v:next.v,t:next.t}}),{});
-    let output = Object.assign({},topHeader,headers,data);
+    return (<><tr><th></th><td></td><td></td><td></td><td></td><td>{`总价:${totalPrice}`}</td></tr></>)
+  },[tableData]);
 
-    let outputPos = Object.keys(output);
-    let ref = outputPos[0]+':'+outputPos[outputPos.length-1];
-
-    let book = xlsx
-        .utils
-        .book_new();
-
-    xlsx
-      .utils
-      .book_append_sheet(book, Object.assign({},output,{'!ref':ref}), "sheet1");
-    xlsx.writeFile(book, `user${new Date().getTime()}.xls`);
-
-  },[tableData])
-
-  const callbackHandler = useCallback((pageData) => {
-    return (<><tr><td><Button onClick={getPdf}>导出pdf</Button></td></tr></>)
-  }, [tableData]);
   return (
     <Modal width={1000}
           title="订单详情"
           visible={isShow}
           onCancel={handleCancel}
-          footer={null}>
-      <Table id="tab" size="small" columns={columns} dataSource={tableData} rowKey='id' summary={callbackHandler} pagination={false}/>
+          footer={<Button type="primary" onClick={getPdf}>导出pdf</Button>}>
+        <Descriptions>
+          <Descriptions.Item label="项目名称">{orderItem.project_name}</Descriptions.Item>
+          <Descriptions.Item label="房号">{orderItem.house_num}</Descriptions.Item>
+          <Descriptions.Item label="户型">{orderItem.style_name}</Descriptions.Item>
+          <Descriptions.Item label="销售名称">{orderItem.sales_name}</Descriptions.Item>
+          <Descriptions.Item label="姓名">{orderItem.master_name}</Descriptions.Item>
+        </Descriptions>
+        <Table size="small" columns={columns} dataSource={tableData} rowKey='id' pagination={false} summary={callbackHandler}/>
     </Modal>
   )
 };
